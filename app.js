cda09999b12be5a0e8aded54d03593449ae18c34
Raegan Millhollin <desdemonhu@gmail.com>
2017-06-08 19:07:44 -0700

    added dataentry.html and requirements to app.js file

---
+++ app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+///Hours of operation 6am - 8pm
+///Daily sales projects based on:
+///Minimum and Max number of customers per hours
+///Average number of cookies per customers
+
+///User requirements
+///Add or remove location
+///Easily change number of cusomters and average cookies based on special circumstances
+
+///Location Object has:
+///Min number of customers
+///Max number of customers
+///Average cookie purchase per customers
+///function: random  number of customers per hour based on min/Max
+///Simulated number of cookies purchased each hours
+///Array of all calculated properties
+
+
+
+///Display in unordered list in Sales.html this way:
+///Location:
+///Hour: Number of cookies
